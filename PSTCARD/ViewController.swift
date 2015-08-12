218741d5fc39cc74d6219cb4c0b7235c7717df87
Chaeny Henry <test@youremail.com>
2015-08-12 20:27:22 +0100

    First Rev OF Project

---
+++ PSTCARD/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PSTCARD
+//
+//  Created by chaeny henry on 09/08/2015.
+//  Copyright (c) 2015 Senzu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func sendMailButtonPressed(sender: AnyObject) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.purpleColor()
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        mailButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+    }
+
+
+}
+
